8706d843bf0a95ffd835883edf077bb0571d73db
Mathieu Renard <renardmat@gmail.com>
2016-07-19 00:48:55 +0200

    Create Xcode project

---
+++ Poker-Stats/Poker-Stats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poker-Stats
+//
+//  Created by Mathieu Renard on 19/07/2016.
+//  Copyright © 2016 Mathieu Renard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
